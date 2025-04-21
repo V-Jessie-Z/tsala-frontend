d0cd60e499abd5b403b454bde3d5699d293cf438
u22757733 <116093000+V-Jessie-Z@users.noreply.github.com>
2025-04-21 09:58:33 +0200

    add news section to home component

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -8,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  selectedNewsItem: any = null;
+
+//News and Annoucements Information
+news = [
+{
+  id:1,
+  title: 'Computer Classes Begin at Tsala',
+  story:'Tsala has evolved and is now introducing computer lessons at the Dema branch. In this age of technology, it is essential for our young stars to be computer literate so they are fully equipped for the outside world. In these classes the students are taught about the basic hardware and software components of computer and how they work. From the CPU to the taskbar on the home screen',
+  imgPath: 'assets/images/Computer-class.jpeg',
+  type: 'Announcement',
+  writer: 'John Doe',
+  date: '12 March 2025'
+},
+{
+  id:2,
+  title: 'Field trip for ECD students',
+  imgPath: 'assets/images/Field-trio.jpeg',
+  date: '01 April 2025'
+},
+
+{
+  id:3,
+  title: ' Sports Awards May 2025',
+  imgPath: 'assets/images/Awards-Day.jpeg',
+  date: '16 May 2025'
+},
+
+]
+
+
+
+//Modal Code
+openModal(item: any) {
+  this.selectedNewsItem = item;
 }
+
+closeModal() {
+  this.selectedNewsItem = null;
+}
+
+}
+
+
+
